49a644d4e337828f35b8d64b6d488cd166fa464b
ker-yaqw <kirill350023@gmail.com>
2022-07-13 01:11:32 +0300

    app v1.0

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,9 +5,9 @@ import './employees-list.css';
 const EmployeesList = () => {
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem name="Jonh C." salary={800} increase={false}/>
-            <EmployeesListItem name="Alex M." salary={3000} increase={true}/>
-            <EmployeesListItem name="Carl W." salary={5000} increase={false}/>
+            <EmployeesListItem name="Jonh C." salary={800} increase={false} onDelete={() => console.log('Deleted')}/>
+            <EmployeesListItem name="Alex M." salary={3000} increase={true} onDelete={() => console.log('Deleted')}/>
+            <EmployeesListItem name="Carl W." salary={5000} increase={false} onDelete={() => console.log('Deleted')}/>
         </ul>
     )
 }
